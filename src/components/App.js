29c951cd2959c5ef1209a9c59a3401ea71b60c6a
NIRAJSHARMA0509 <120631030+NIRAJSHARMA0509@users.noreply.github.com>
2023-01-05 11:44:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/App.css";
 import Component from "./Component";
 import ErrorBoundary from "./ErrorBoundary";
 const App = () => {
 
+  const [errorStatus, setErrorStatus] = useState(false);
+ 
+  const errorGenerator = () => {
+    
+    setErrorStatus(true);
+  }
 
   return (
 
     <div id="main">
     <h2>React Error Boundaries</h2>
-      <ErrorBoundary>
-        <Component change={changeFunc} />
+      <ErrorBoundary  >
+       {errorStatus && <Component change={errorStatus} />}
       </ErrorBoundary>
       <button id="gen" onClick={errorGenerator}>Generate Error</button>
     </div>
